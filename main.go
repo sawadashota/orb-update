6c1704c920eeb86dc96f5d88a166bc5fad61edb9
Shota Sawada <xiootas@gmail.com>
2019-12-08 21:21:40 +0900

    [from now] 2019/12/08 21:21:40
    diff --git a/main.go b/main.go
    index 989946f..6bfeaa1 100644
    --- a/main.go
    +++ b/main.go
    @@ -1,15 +1,81 @@
     package main
    
     import (
    -	"fmt"
     	"os"
    +	"time"
    
    -	"github.com/sawadashota/orb-update/cmd"
    +	"gopkg.in/src-d/go-git.v4/plumbing/object"
    +
    +	"gopkg.in/src-d/go-git.v4/plumbing"
    +
    +	"gopkg.in/src-d/go-git.v4"
     )
    
     func main() {
    -	if err := cmd.RootCmd().Execute(); err != nil {
    -		_, _ = fmt.Fprintln(os.Stderr, err)
    -		os.Exit(1)
    +
    +	_, err := run()
    +	if err != nil {
    +		panic(err)
    +	}
    +	//if err := cmd.RootCmd().Execute(); err != nil {
    +	//	_, _ = fmt.Fprintln(os.Stderr, err)
    +	//	os.Exit(1)
    +	//}
    +}
    +
    +func run() ([]byte, error) {
    +	// open
    +	pwd, err := os.Getwd()
    +	if err != nil {
    +		return nil, err
    +	}
    +
    +	repo, err := git.PlainOpen(pwd)
    +	if err != nil {
    +		return nil, err
    +	}
    +
    +	w, err := repo.Worktree()
    +	if err != nil {
    +		return nil, err
     	}
    +
    +	// checkout
    +	branch := "test-" + time.Now().String()
    +	err = w.Checkout(&git.CheckoutOptions{
    +		Branch: plumbing.ReferenceName(branch),
    +		Create: true,
    +	})
    +	if err != nil {
    +		return nil, err
    +	}
    +
    +	// create file
    +	f, err := os.Create(".test.txt")
    +	if err != nil {
    +		return nil, err
    +	}
    +	f.Write([]byte("hello\n"))
    +	f.Close()
    +
    +	// commit
    +	ref := plumbing.ReferenceName(branch)
    +	_, err = w.Add(".version")
    +	if err != nil {
    +		return nil, err
    +	}
    +
    +	hash, _ := w.Commit("update version", &git.CommitOptions{
    +		Author: &object.Signature{
    +			Name:  "Shota Sawada",
    +			Email: "xiootas@gmail.com",
    +			When:  time.Now(),
    +		},
    +	})
    +	err = repo.Storer.SetReference(plumbing.NewReferenceFromStrings(branch, hash.String()))
    +	if err != nil {
    +		return nil, err
    +	}
    +
    +	return nil, nil
     }
    diff --git a/pullrequest/github.go b/pullrequest/github.go
    index 248f81f..bca5796 100644
    --- a/pullrequest/github.go
    +++ b/pullrequest/github.go
    @@ -76,5 +76,5 @@ func (g *GitHubPullRequest) Create(ctx context.Context, message string) error {
    
     func (g *GitHubPullRequest) branch() string {
     	o := g.difference.New
    -	return fmt.Sprintf("orb-update/%s/%s-%s-aaaaaaaaaa", o.Namespace(), o.Name(), o.Version())
    +	return fmt.Sprintf("orb-update/%s/%s-%s-aaaaaaaaaaa", o.Namespace(), o.Name(), o.Version())
     }

--- main.go
+++ main.go
@@ -1,15 +1,81 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"time"
 
-	"github.com/sawadashota/orb-update/cmd"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+
+	"gopkg.in/src-d/go-git.v4/plumbing"
+
+	"gopkg.in/src-d/go-git.v4"
 )
 
 func main() {
-	if err := cmd.RootCmd().Execute(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+
+	_, err := run()
+	if err != nil {
+		panic(err)
 	}
+	//if err := cmd.RootCmd().Execute(); err != nil {
+	//	_, _ = fmt.Fprintln(os.Stderr, err)
+	//	os.Exit(1)
+	//}
+}
+
+func run() ([]byte, error) {
+	// open
+	pwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	repo, err := git.PlainOpen(pwd)
+	if err != nil {
+		return nil, err
+	}
+
+	w, err := repo.Worktree()
+	if err != nil {
+		return nil, err
+	}
+
+	// checkout
+	branch := "test-" + time.Now().String()
+	err = w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName(branch),
+		Create: true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// create file
+	f, err := os.Create(".test.txt")
+	if err != nil {
+		return nil, err
+	}
+	f.Write([]byte("hello\n"))
+	f.Close()
+
+	// commit
+	ref := plumbing.ReferenceName(branch)
+	_, err = w.Add(".version")
+	if err != nil {
+		return nil, err
+	}
+
+	hash, _ := w.Commit("update version", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Shota Sawada",
+			Email: "xiootas@gmail.com",
+			When:  time.Now(),
+		},
+	})
+	err = repo.Storer.SetReference(plumbing.NewReferenceFromStrings(branch, hash.String()))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
