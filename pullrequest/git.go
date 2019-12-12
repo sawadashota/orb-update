fd1d94b547cbbd8484c705c4d0b9ca6dd27560cc
Shota Sawada <xiootas@gmail.com>
2019-12-12 21:16:33 +0900

    [from now] 2019/12/12 21:16:32
    diff --git a/pullrequest/git.go b/pullrequest/git.go
    index 8d910c6..f3b8c31 100644
    --- a/pullrequest/git.go
    +++ b/pullrequest/git.go
    @@ -85,8 +85,10 @@ func (d *DefaultGitClient) SwitchBack() error {
     		return err
     	}
    
    +	fmt.Printf("SwitchBack: %s", d.BaseBranch())
     	return w.Checkout(&git.CheckoutOptions{
     		Branch: plumbing.ReferenceName("refs/heads/" + d.BaseBranch()),
    +		Force:  true,
     	})
     }

--- pullrequest/git.go
+++ pullrequest/git.go
@@ -85,8 +85,10 @@ func (d *DefaultGitClient) SwitchBack() error {
 		return err
 	}
 
+	fmt.Printf("SwitchBack: %s", d.BaseBranch())
 	return w.Checkout(&git.CheckoutOptions{
 		Branch: plumbing.ReferenceName("refs/heads/" + d.BaseBranch()),
+		Force:  true,
 	})
 }
 
